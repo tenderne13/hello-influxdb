1668bd0fef35f1a8a5d9cc0a05bf261553d86907
lixp <lixp7@xiaopeng.com>
2025-01-10 10:16:25 +0800

    init

---
+++ src/main/java/com/xiaopeng/ai/influxdb/HelloInfluxdbApplication.java
@@ -0,0 +1,13 @@
+package com.xiaopeng.ai.influxdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloInfluxdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloInfluxdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaopeng/ai/influxdb/HelloInfluxdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaopeng.ai.influxdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloInfluxdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
